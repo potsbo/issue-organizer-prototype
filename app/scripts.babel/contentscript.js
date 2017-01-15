c56fa99af0393c44a19527ba11f104845575c17c
Shimpei Otsubo <potsbo@gmail.com>
2017-01-15 21:37:12 +0900

    Make angularize

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,19 +1,22 @@
 'use strict'
 
 window.addEventListener('load', () => {
-  const html = document.querySelector('html')
-  html.setAttribute('ng-app', '')
-  html.setAttribute('ng-csp', '')
+  const angularize = () => {
+    const html = document.querySelector('html')
+    html.setAttribute('ng-app', '')
+    html.setAttribute('ng-csp', '')
 
-  const header = document.querySelector('.header')
-  header.setAttribute('ng-controller', 'MainController')
+    const header = document.querySelector('.header')
+    header.setAttribute('ng-controller', 'MainController')
 
-  const types = ['pulls', 'issues']
-  types.forEach((type) => {
-    const query = `.header-nav .header-nav-item a[href='/${type}']`
-    const item = document.querySelector(query)
-    item.setAttribute('href', `/${type}{{ query.${type} }}`)
-  })
+    const types = ['pulls', 'issues']
+    types.forEach((type) => {
+      const query = `.header-nav .header-nav-item a[href='/${type}']`
+      const item = document.querySelector(query)
+      item.setAttribute('href', `/${type}{{ query.${type} }}`)
+    })
+  }
+  angularize()
 
   const app = angular.module('GitHubIssueOrganizer', [])
   app.controller('MainController', ($scope) => {
@@ -32,5 +35,5 @@ window.addEventListener('load', () => {
     }
   })
 
-  angular.bootstrap(html, ['GitHubIssueOrganizer'], [])
+  angular.bootstrap(document.querySelector('html'), ['GitHubIssueOrganizer'], [])
 })
