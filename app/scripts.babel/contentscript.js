7827aee6df3cbae5ed6a6f4a6724d6377caa4e36
Shimpei Otsubo <potsbo@gmail.com>
2017-01-15 21:48:21 +0900

    Use githubController

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -15,25 +15,8 @@ window.addEventListener('load', () => {
       const item = document.querySelector(query)
       item.setAttribute('href', `/${type}{{ query.${type} }}`)
     })
+
+    angular.bootstrap(document.querySelector('html'), ['GitHubIssueOrganizer'], [])
   }
   angularize()
-
-  const app = angular.module('GitHubIssueOrganizer', [])
-  app.controller('MainController', ($scope) => {
-    const queryFor = (type) => {
-      // TODO: hard code here
-      const user = 'potsbo'
-      const org = 'wantedly'
-      const q = `q=is%3Aopen+is%3A${type}+author%3A${user}+org%3A${org}`
-      const utf8 = 'utf8=✓'
-      return `?${utf8}&${q}`
-    }
-
-    $scope.query = {
-      pulls: queryFor('pr'),
-      issues: queryFor('issue')
-    }
-  })
-
-  angular.bootstrap(document.querySelector('html'), ['GitHubIssueOrganizer'], [])
 })
