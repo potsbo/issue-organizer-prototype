9ac915aee09b0bba6c9b5cbff28fb302a7c54cbc
Shimpei Otsubo <potsbo@gmail.com>
2017-01-15 21:50:27 +0900

    Deangularinze

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,22 +1,19 @@
 'use strict'
 
 window.addEventListener('load', () => {
-  const angularize = () => {
-    const html = document.querySelector('html')
-    html.setAttribute('ng-app', '')
-    html.setAttribute('ng-csp', '')
+  const html = document.querySelector('html')
+  html.setAttribute('ng-app', '')
+  html.setAttribute('ng-csp', '')
 
-    const header = document.querySelector('.header')
-    header.setAttribute('ng-controller', 'GitHubHeaderController')
+  const header = document.querySelector('.header')
+  header.setAttribute('ng-controller', 'GitHubHeaderController')
 
-    const types = ['pulls', 'issues']
-    types.forEach((type) => {
-      const query = `.header-nav .header-nav-item a[href='/${type}']`
-      const item = document.querySelector(query)
-      item.setAttribute('href', `/${type}{{ query.${type} }}`)
-    })
+  const types = ['pulls', 'issues']
+  types.forEach((type) => {
+    const query = `.header-nav .header-nav-item a[href='/${type}']`
+    const item = document.querySelector(query)
+    item.setAttribute('href', `/${type}{{ query.${type} }}`)
+  })
 
-    angular.bootstrap(document.querySelector('html'), ['GitHubIssueOrganizer'], [])
-  }
-  angularize()
+  angular.bootstrap(document.querySelector('html'), ['GitHubIssueOrganizer'], [])
 })
